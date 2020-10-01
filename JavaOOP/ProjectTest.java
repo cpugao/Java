a1cfbe19aff5172a2d0668d2d8f1b5c220509232
Carlo Pugao <cpugao@yahoo.com>
2020-10-01 00:09:49 -0700

    finished project class

---
+++ JavaOOP/ProjectTest.java
@@ -0,0 +1,6 @@
+class ProjectTest {
+    public static void main(String[] args) {
+        Project project = new Project("Java Project", "Objects and Classes", 20);
+        System.out.println(project.elevatorPitch());
+    }
+}
