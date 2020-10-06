3d1bac5e7e4d45ac7e3f7ff6e49e5c6efc530bfc
Carlo Pugao <cpugao@yahoo.com>
2020-10-05 21:30:38 -0700

    java routing assigment

---
+++ familiarwithrouting/src/main/java/com/carlopugao/familiarwithrouting/FamiliarwithroutingApplication.java
@@ -0,0 +1,13 @@
+package com.carlopugao.familiarwithrouting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamiliarwithroutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamiliarwithroutingApplication.class, args);
+	}
+
+}
---
+++ familiarwithrouting/src/test/java/com/carlopugao/familiarwithrouting/FamiliarwithroutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlopugao.familiarwithrouting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamiliarwithroutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
