adf4968aa93f942e33c5eb965acbb7cf732e9bb7
Carlo Pugao <cpugao@yahoo.com>
2020-10-09 04:02:42 -0700

    update and delete

---
+++ mvc/src/main/java/com/carlopugao/mvc/services/BookService.java
@@ -0,0 +1,51 @@
+package com.carlopugao.mvc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.carlopugao.mvc.models.Book;
+import com.carlopugao.mvc.repositories.BookRepository;
+
+@Service
+public class BookService {
+	private final BookRepository bookRepository;
+	
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+	
+	public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+
+	public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+		Book b = this.findBook(id);
+		b.setTitle(title);;
+		b.setDescription(desc);
+		b.setLanguage(lang);
+		b.setNumberOfPages(numOfPages);
+		return bookRepository.save(b);
+	}
+
+	public void deleteBook(Long id) {
+		bookRepository.deleteById(id);
+		
+	}
+	
+	
+}
