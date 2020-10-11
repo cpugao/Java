f5cfa06e3a36421830b5d8aad647fb324c545ad0
Carlo Pugao <cpugao@yahoo.com>
2020-10-11 04:47:48 -0700

    updated mvc(books)

--- mvc/src/main/java/com/carlopugao/mvc/services/BookService.java
+++ mvc/src/main/java/com/carlopugao/mvc/services/BookService.java
@@ -3,6 +3,8 @@ package com.carlopugao.mvc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Service;
 
 import com.carlopugao.mvc.models.Book;
@@ -45,6 +47,10 @@ public class BookService {
 	public void deleteBook(Long id) {
 		bookRepository.deleteById(id);
 		
+	}
+
+	public Book updateBook(Book book) {
+		return bookRepository.save(book);
 	}
 	
 	
